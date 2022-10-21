3f5aac36d197d02d7d34e3329ee2946949e775c4
gtouya <guillaume.touya@ign.fr>
2022-10-21 15:39:11 +0200

    Create spectral_saliency.py

---
+++ saliency/spectral_saliency.py
@@ -0,0 +1,28 @@
+# This file shows how to use the spectral saliency model from Hou & Zhang (2007), implemented in the OpenCV library. 
+# There are two versions, a basic one and a fine grained one.
+
+import cv2
+from skimage import io, data_dir
+import os
+import numpy as np
+
+file = os.path.join(data_dir, 'D:\Donnees\map_test.jpg') 
+map = io.imread(file)
+
+saliency = cv2.saliency.StaticSaliencySpectralResidual_create()
+(success, saliencyMap) = saliency.computeSaliency(map)
+saliencyMap = (saliencyMap * 255).astype("uint8")
+
+saliency2 = cv2.saliency.StaticSaliencyFineGrained_create()
+(success, saliencyMap2) = saliency2.computeSaliency(map)
+
+threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255,
+	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+
+
+cv2.imshow("Image", map)
+cv2.imshow("Output", threshMap)
+cv2.waitKey(0)
+
+io.imsave('D:\Donnees\map_test_saliency3.jpg',threshMap)
